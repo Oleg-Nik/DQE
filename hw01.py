83b6d0651717463976ada0f2bba54779851b5cd9
Oleg_Krylov <krylov_o@mail.ru>
2021-02-10 17:03:02 +0300

    comments added

--- hw01.py
+++ hw01.py
@@ -1,14 +1,16 @@
 #
 import random
-list_length = 100; probability_range = 1000 # define Length of the list and the Depth of probability
+list_length = 100
+probability_range = 1000 # define Length of the list and the Depth of probability
 sorted_list = list(range(list_length)) # create dummy for sorted list
 list_of_random_numbers = list(range(list_length)) # create dummy for random list
 # create list with random number
 for _ in range(list_length): list_of_random_numbers[_] = random.randint(1, probability_range)
-# comprehension using: list_of_random_numbers_compr_using = [random.randint(1, probability_range) for i in range(list_length)]
+# comprehension using:
+# list_of_random_numbers_compr_using = [random.randint(1, probability_range) for i in range(list_length)]
 print("------->: unsorted random List:")
 print(list_of_random_numbers)
-#sorting the List
+# sorting the List
 for i in range(list_length):
     min_number = min(list_of_random_numbers)
     sorted_list[i] = min_number
@@ -19,8 +21,10 @@ print(sorted_list)
 # Sums calculation
 even_sum = 0; odd_sum = 0
 for i in range(list_length):
-    if sorted_list[i] % 2 == 0: even_sum = even_sum + sorted_list[i]
+    if sorted_list[i] % 2 == 0:
+        even_sum = even_sum + sorted_list[i]
 for i in range(list_length):
-    if sorted_list[i] % 2 == 1: odd_sum = odd_sum + sorted_list[i]
+    if sorted_list[i] % 2 == 1:
+        odd_sum = odd_sum + sorted_list[i]
 print("The sum of even numbers:", even_sum)
-print("The sum of odd numbers:", odd_sum) # test
+print("The sum of odd numbers:", odd_sum)
