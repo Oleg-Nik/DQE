845cd3b972f19ca25aa23f30476e7a85c1cc62a1
Oleg_Krylov <krylov_o@mail.ru>
2021-02-16 17:43:19 +0300

    list-comprehension added, prints changed

--- hw01.py
+++ hw01.py
@@ -1,5 +1,5 @@
-#
 import random
+
 # define Length of the list and the Depth of probability
 list_length = 100
 probability_range = 1000
@@ -7,30 +7,20 @@ probability_range = 1000
 sorted_list = []
 list_of_random_numbers = []
 # creation list with random numbers
-for _ in range(100):
-    list_of_random_numbers[_] = random.randint(1, probability_range)
-print("------->: unsorted random List:")
-print(list_of_random_numbers)
+for _ in range(list_length):
+    list_of_random_numbers.append(random.randint(1, probability_range))
+# test = [random.randint(1, probability_range) for _ in range(list_length)]
+# an output for random lists
+print("------->: unsorted random List:\n", list_of_random_numbers)
 # sorting the List
-for i in range(list_length):
+for _ in range(list_length):
     min_number = min(list_of_random_numbers)
-    sorted_list[i] = min_number
+    sorted_list.append(min_number)
     list_of_random_numbers.remove(min_number)
-# an output for random and sorted lists
-print("-------> sorted random List:")
-print(sorted_list)
+# an output for sorted lists
+print("-------> sorted random List:\n", sorted_list)
 # Averages calculation
-even_sum = 0
-even_average_count = 0
-odd_sum = 0
-odd_average_count = 0
-for i in range(list_length):
-    if sorted_list[i] % 2 == 0:
-        even_sum = even_sum + sorted_list[i]
-        even_average_count = even_average_count + 1
-for i in range(list_length):
-    if sorted_list[i] % 2 == 1:
-        odd_sum = odd_sum + sorted_list[i]
-        odd_average_count = odd_average_count + 1
-print("The average for even number:", round(even_sum/even_average_count))
-print("The average for odd number:", round(odd_sum/odd_average_count))
+even_list = [val for val in sorted_list if val % 2 == 0]
+odd_list = [val for val in sorted_list if val % 2 == 1]
+print("The average for even number:", round(sum(even_list)/len(even_list)))
+print("The average for odd number:", round(sum(odd_list)/len(odd_list)))
